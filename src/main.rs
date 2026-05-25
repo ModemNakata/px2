4e72ceabada9bd1badb4ad26a54db1669badb614
Nakata Modem <nakatamodem@hotmail.com>
2026-05-25 13:18:38 +0200

    bun + react + vite + react compiler + typescript + nginx

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,13 @@
-use actix_files as fs;
-use actix_web::{App, HttpServer, Responder, Result, middleware, web};
-use askama::Template;
-
-#[derive(Template)]
-#[template(path = "dist/index.html")]
-struct Index;
-
-async fn index() -> Result<impl Responder> {
-    let html = Index.render().expect("template should be valid");
-
-    Ok(web::Html::new(html))
-}
+use actix_web::{App, HttpServer, middleware};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 
-    log::info!("starting HTTP server at http://localhost:8080");
+    log::info!("starting HTTP server");
 
-    HttpServer::new(move || {
-        App::new()
-            .wrap(middleware::Logger::default())
-            .service(web::resource("/").route(web::get().to(index)))
-            .service(fs::Files::new("/", "./sj/dist").index_file("index.html"))
-    })
-    .bind(("127.0.0.1", 3715))?
-    .run()
-    .await
+    HttpServer::new(move || App::new().wrap(middleware::Logger::default()))
+        .bind(("127.0.0.1", 9291))?
+        .run()
+        .await
 }
