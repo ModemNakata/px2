f8c7fc52ec5a6713ebbdf8f4b0bf410a3e8ca076
Nakata Modem <nakatamodem@hotmail.com>
2026-06-03 17:16:21 +0200

    base sea-orm connection through appstate

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,36 @@
-use actix_web::{App, HttpResponse, HttpServer, get, middleware};
-use chrono::Utc;
-use serde_json;
+use actix_web::{App, HttpServer, middleware, web};
+use sea_orm::{Database, DatabaseConnection}:
+use std::env;
 
 mod auth;
 
-#[get("/datetime")]
-async fn datetime() -> HttpResponse {
-    HttpResponse::Ok().json(serde_json::json!({
-        "now": Utc::now().to_rfc3339()
-    }))
+#[derive(Debug, Clone)]
+struct AppState {
+    conn: DatabaseConnection,
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+    // tracing_subcriber-
+
+    let db_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file"); // must be set
+
+    // ... must be set
+
+    let conn = Database::connect(&db_url).await.unwrap();
+    // Migrator::up(&conn, None).await.unwrap() /// Migrator (?) (???) ?
+
+    let state = AppState { conn };
 
     log::info!("starting HTTP server");
 
     HttpServer::new(move || {
         App::new()
+            .app_data(web::Data::new(state.clone()))
             .wrap(middleware::Logger::default())
-            .service(datetime)
+            // .service()
+            // .default_service(web::route().to(not_found))
             .configure(auth::configure_auth_routes)
     })
     .bind(("0.0.0.0", 9291))? // public facing
